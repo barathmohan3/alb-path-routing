5d67fdcd662f62ee0faa3512fdc3e8717c104f2a
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-05-30 15:04:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "my-oidc-bucket-15328069840"
     key            = "alb-path-routing/terraform.tfstate"
     profile        = "bmware"
-    region         = "eu-west-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
