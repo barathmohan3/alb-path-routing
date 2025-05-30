dd8ba4dae0c015cc2b31a14b15bad69e5e8ada72
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-05-30 14:43:42 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "s3-github"
     key            = "alb-path-routing/terraform.tfstate"
-    region         = "us-east-1"
     profile        = "bmware"
     dynamodb_table = "terraform-locks"
     encrypt        = true
